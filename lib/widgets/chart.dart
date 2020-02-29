9e9ec2cc9d8aed8a6936229cb918dba888bcf54e
saffer4u <aftab.alam4u71@gmail.com>
2020-02-29 23:52:01 +0530

    Transaction list with ListTile widget

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -45,7 +45,7 @@ class Chart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print(groupedTransactioValues);
+    // print(groupedTransactioValues);
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
